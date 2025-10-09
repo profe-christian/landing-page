687461a1fb16732b58e8da0fa6322bfa62f39a05
Christian <chr.acunas@profesor.duoc.cl>
2025-10-09 20:03:37 -0300

    template actualizado

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
+const data = [
+  { id: "api-spring", title: "API REST en Java Spring", price: 750000 },
+  { id: "web-python", title: "Web Full-Stack con Python", price: 680000 },
+  { id: "dba-oracle", title: "Soporte & DBA Oracle", price: 540000 },
+  { id: "erp-impl", title: "Implementación de ERP", price: 1150000 },
+  { id: "kit-devops", title: "Kit DevOps Monitoring", price: 320000 },
+];
+
 export const Products = () => {
   return (
     <>
-      <h1>Productos</h1>
+      <section className="container">
+        <h1>Productos</h1>
+      </section>
     </>
   );
 };
